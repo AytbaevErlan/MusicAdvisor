62574695730c183108837b99716c3e4ad9e10315
hyper <hyperskill@hyperskill.org>
2024-10-07 13:41:51 +0200

    Test

--- Music Advisor/task/src/advisor/Test.java
+++ Music Advisor/task/src/advisor/Test.java
@@ -2,6 +2,6 @@ package advisor;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("This is a test class");
+        System.out.println("This is a test class - still not working(checked)");
     }
 }
